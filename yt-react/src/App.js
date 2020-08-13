180a93382e66cc99abbc3092605693bbf12ba6b2
Gabriel Amorim <gaahamorim95@gmail.com>
2020-08-13 15:56:04 -0300

    feat(App): Routes to /repos/

--- yt-react/src/App.js
+++ yt-react/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Repositories from "./containers/Repositories"
+import Repository from "./containers/Repository"
 
 function App(){
   return (
-    <div className="App">
-      <Repositories />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route path="/repos/:owner/:name" component={Repository} />
+          <Route path="/" component={Repositories} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- yt-react/src/containers/Repositories.jsx
+++ yt-react/src/containers/Repositories.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { listRepositories } from '../api/github';
 import Item from '../components/Item'
 
